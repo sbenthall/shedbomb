9859118414ecf45a32fdeed976a9018dc74b5cf6
Sebastian Benthall <sbenthall@gmail.com>
2010-04-29 20:24:23 -0400

    persisting questions in mongo

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from mongokit import *
 
 app = Flask(__name__)
+
+
+class Bikeshed(Document):
+    structure = {
+        'question':unicode
+        }
+    # required_fields = ['question']
+    # default_values = {'question':u''}    
+
+con = Connection()
+con.register([Bikeshed])
+
+shedbombdb = con.shedbomb
+bikesheds = shedbombdb.bikesheds
 
 @app.route('/')
 def index():
@@ -10,11 +25,15 @@ def index():
 
 @app.route('/bomb',methods=['GET','POST'])
 def bomb():
-
     if request.method == 'POST':
         question=request.form['question']
     else:
         question=request.args.get('question','Did you forget to ask a question?')
+
+    bikesheds.Bikeshed({
+            'question': question
+            }).save()
+
     return render_template('bomb.html',question=question)
 
 if __name__ == '__main__':
